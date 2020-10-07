c891c57e454df9ea7deac2d4c209c260e7526deb
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-10-07 00:14:35 -0700

    function and json file created

--- user.js
+++ user.js
@@ -1,3 +1,19 @@
 const axios = require('axios');
 const fs = require('fs');
 const url = 'https://jsonplaceholder.typicode.com/users';
+
+async function getData(url) {
+    try {
+        const response = await axios.get(url);
+        const data = await response.data;
+        const newData = await JSON.stringify(data);
+        fs.writeFileSync('addressList.json', newData, (err) => {
+            if(err) throw(err);
+            console.log('addressList.json created successfully');
+        })
+    }
+    catch(err) {
+        console.log('Something went wrong!');
+    }
+};
+getData(url);
