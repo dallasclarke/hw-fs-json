2ddaa42e8a936bb6569a231e67dc5de19a6a4bc1
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-10-08 14:01:32 -0700

    Done

--- addressList.js
+++ addressList.js
@@ -1,10 +1,44 @@
+// const { EEXIST } = require('constants');
 const fs = require('fs');
 
 function gatherInfo() {
+    console.log('*****Address List*****')
     fs.readFile('addressList.json', 'utf-8', (err, info) => {
-        if(err) throw err;
-        else if(!info.data) {
+        // console.log(info)
+        if(err) return console.log(err);
+        else if(!info) {
             console.log('There is no data in the file!!!');
         }
+        else {
+            fs.readFile('addressList.json', 'utf-8', (err, info) => {
+                if(err) return console.log(err);
+                const data = JSON.parse(info);
+                // console.log(data)
+                let newData = '';
+                data.forEach((info) => {
+                    let {
+                        name,
+                        email,
+                        address: {street, suite, city, zipcode},
+                        phone
+                    } = info
+
+                    newData += 
+                    `\n${name}\n
+                    Address: ${street}, ${suite}
+                    ${city}, ${zipcode}
+                    Phone: ${phone}
+                    Email: ${email}`
+                    
+                })
+                fs.writeFile('addressList.txt', newData, (err) => {
+                    if (err) return console.log(err);
+                    console.log('Data saved to file')
+                })
+            })
+            
+        }
+        
     })
 }
+gatherInfo()
